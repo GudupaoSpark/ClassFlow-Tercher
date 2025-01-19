2e4a3ba27ae1858cb7ff585efa4d6eec22b65133
lidongxun967 <1985409711@qq.com>
2025-01-19 17:29:49 +0800

    feat(const): 添加深浅主题并根据系统自动切换
    - 新增深色和浅色主题颜色配置
    - 使用 darkdetect 模块检测系统主题
    - 根据系统主题自动选择应用主题

--- const.py
+++ const.py
@@ -1,6 +1,7 @@
 import os
+import darkdetect
 
-theme_colors = {
+theme_colors_dark = {
     "primary": "#60CDFF",
     "secondary": "#2B2B2B",
     "surface": "#202020",
@@ -11,7 +12,19 @@ theme_colors = {
     "hover": "#2D2D2D",
     "selected": "#3B3B3B",
     "bgcolor": "#101010",
-    
+}
+
+theme_colors_light = {
+    "primary": "#42A5F5",
+    "secondary": "#F5F5F5",
+    "surface": "#FFFFFF",
+    "background": "#ECEFF1",
+    "text": "#000000",
+    "text_secondary": "#757575",
+    "border": "#BDBDBD",
+    "hover": "#E0E0E0",
+    "selected": "#BBDEFB",
+    "bgcolor": "#FAFAFA",
 }
 
 if os.name == "nt":
@@ -20,6 +33,8 @@ else:
     data_dir = os.getenv("HOME")+"/.ClassFlow/Tercher"
 
 assets_dir = os.path.abspath(".")+"/assets"
+
+theme_colors = theme_colors_light if darkdetect.theme() == "Light" else theme_colors_dark
 
 if not os.path.exists(data_dir):
     os.makedirs(data_dir)
